ce7aaa5cc493f015474c64caca99e5c65800afcb
Olga Polyakowa <polyakowa.o2011@yandex.ru>
2020-12-16 19:00:31 +0400

    2nd task

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -6,4 +6,22 @@
  */
 export function sortStrings(arr, param = 'asc') {
 
+    let arrClone = arr.slice();
+
+    let sort = arrClone.sort((a,b)=> a.localeCompare(b,['ru-RU','en-UA'],{ caseFirst: 'upper'}));
+    
+        if (param == 'asc') {
+
+           return sort; 
+        
+
+       } else if (param == 'desc'){
+                     
+    let sortDesc = sort.reverse();
+
+           return sortDesc;
+         
+
+     } else alert("ТАК сортировать не обучены! Либо 'ask' либо 'desc'!")
+
 }
