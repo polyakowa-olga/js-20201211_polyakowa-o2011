ce988efdb88bfedd8eae08e233e49b6c77a9f269
Olga Polyakowa <polyakowa.o2011@yandex.ru>
2020-12-16 22:44:08 +0400

    4th task

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,14 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
+    
+    let clone ={};
 
+    for (let [key, value] of Object.entries(obj) ) {
+       
+        if ( !fields.includes(key) ) {
+           clone[ key ] = value;
+        }     
+    }
+       return clone;
 };
